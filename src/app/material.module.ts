28c191c0aed8c0bb53f8c9aa95ae6fbe783c9948
Simon Daniel <sdaniel1609@gmail.com>
2018-12-12 06:46:12 +0000

    added resource file and navigation component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,27 @@ import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-  imports: [BrowserAnimationsModule, MatBadgeModule, MatButtonModule],
-  exports: [BrowserAnimationsModule, MatBadgeModule, MatButtonModule]
+  imports: [BrowserAnimationsModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule],
+
+  exports: [BrowserAnimationsModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule]
 })
 
 
