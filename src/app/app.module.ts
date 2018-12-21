042eb64e54b2dc727d814972c8bee7a021b60de4
Simon Daniel <sdaniel1609@gmail.com>
2018-12-21 16:25:59 +0000

    added logo, mat-tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { NavtabsComponent } from './navigation/navtabs/navtabs.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -55,7 +56,8 @@ import { NavtabsComponent } from './navigation/navtabs/navtabs.component';
   imports: [
     BrowserModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material';
 
 @NgModule({
   imports: [BrowserAnimationsModule,
@@ -14,7 +15,8 @@ import {MatListModule} from '@angular/material/list';
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
-    MatListModule],
+    MatListModule,
+    MatTabsModule],
 
   exports: [BrowserAnimationsModule,
     MatBadgeModule,
@@ -22,7 +24,8 @@ import {MatListModule} from '@angular/material/list';
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
-    MatListModule]
+    MatListModule,
+    MatTabsModule]
 })
 
 
